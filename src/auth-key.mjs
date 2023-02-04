2fbac63aa74b9d2fa317c932caf3643a76f235f1
Wany <sung@wany.io>
2023-02-04 13:57:07 +0000

    add key code

--- src/auth-key.mjs
+++ src/auth-key.mjs
@@ -9,7 +9,7 @@ export default class AuthKey extends MySQLClass {
     super(mysql);
 
     this.element = element;
-    this.code = new Crypto().uid();
+    this.code = Crypto().randomString(42);
     this.account = this.element.uid;
     this.expire = new Date(0);
 
